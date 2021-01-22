1ff78556011bcec7ae3e7631d742a2441e640030
Philippe SEGATORI <p.segatori@youstock.com>
2021-01-22 11:47:19 +0100

    Add image to items

--- src/Interfaces/Item.php
+++ src/Interfaces/Item.php
@@ -11,18 +11,8 @@ namespace iTunesPodcastFeed\Interfaces;
 
 interface Item extends Xml
 {
-    /**
-     * Item constructor.
-     * @param string $title
-     * @param string $fileUrl
-     * @param string $duration For example: 2:29
-     * @param string $description
-     * @param int $date Since unix epoch
-     * @param int $fileSizeBytes
-     * @param string $mime
-     */
     public function __construct(
         string $title, string $fileUrl, string $duration, string $description,
-        int $date, int $fileSizeBytes, string $mime
+        int $date, int $fileSizeBytes, string $mime, string $image
     );
 }
